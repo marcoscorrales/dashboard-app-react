c99615fe5fa3b4f482eb4d7832b41e5b7a4bb8ae
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-11-07 19:24:17 +0100

    Termino el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,11 @@ import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
+
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) =>(
   <TooltipComponent content={ title } position="BottomCenter">
@@ -13,20 +17,62 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) =>(
 
       <span style={{ background: dotColor }}
        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
+      />
         {icon}
-      </span>
-
     </button>
   </TooltipComponent>
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+  },[]);
+
+  useEffect(() => {
+    if(screenSize <= 900){
+      setActiveMenu(false);
+    }else{
+      setActiveMenu(true);
+    } 
+  }, [screenSize]);
 
   return (
     <div className='flex justify-between p-2 md:mx-6'>
       <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu/>}/>
+
+      <div className='flex'>
+      <NavButton title="Cart" customFunc={() => handleClick('cart')} color="blue" icon={<FiShoppingCart/>}/>
+
+      <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color="blue" icon={<BsChatLeft/>}/>
+
+      <NavButton title="Notifications" dotColor="#03C9D7" customFunc={() => handleClick('notofication')} color="blue" icon={<RiNotification3Line/>}/>
+
+      <TooltipComponent
+      content="Profile"
+      position="BottomCenter"
+      >
+        <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
+        onClick={() => handleClick('userProfile')}>
+          <img className='rounded-full w-8 h8' src={avatar}/>
+          <p>
+            <span className='text-gray-400 text-14'>Hi, </span>{' '}
+            <span className='text-gray-400 font-bold ml-1 text-14'>Marcos</span>
+          </p>
+          <MdKeyboardArrowDown className='text-gray-400 text-14'/>
+        </div>
+      </TooltipComponent>
+
+      {isClicked.cart && <Cart />}
+      {isClicked.chat && <Chat />}
+      {isClicked.notification && <Notification />}
+      {isClicked.userProfile && <UserProfile />}
+      </div>
     </div>
   )
 }
